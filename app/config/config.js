51aeb8b1928fd0024353e7e16db8e16bbda30ed8
yevheniydomin <yevhen.domin.uk@gmail.com>
2023-04-16 15:31:30 +0100

    db config has been tuned

--- app/config/config.js
+++ app/config/config.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     database: 'dev',
+    password: null,
     dialect: "sqlite",
     storage: "../data/dev.db"
   },
   test: {
+    username: 'root',
+    password: null,
     database: 'test',
     dialect: "sqlite",
     storage: "../data/test.db"
@@ -14,6 +19,7 @@ module.exports = {
     password: process.env.SQLITE_PASSWORD,
     database: process.env.SQLITE_DATABASE,
     dialect: "sqlite",
-    storage: "../data/production.db"
+    storage: "../../data/production.db",
+    port: 5432
   }
 }
